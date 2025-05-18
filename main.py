ff123d672cfe8b7495d8f2380898cc02eb0639cb
gchandel <32592803+gchandel@users.noreply.github.com>
2025-05-18 21:21:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#Python is a high level (human readable) (Layer of Low Level Language which is machine code post compilation by compiler), 
+#interpreted programming language (there is no compiler so no need to compiler, interpretor will convert into byte code (low level) 
+# which will further run on hardware, it will run line by line) 
+# known for its dynamic typing, 
+# simplicity (clean code, easy readibility)
+# versatility (can be used by many places as by DE, DA, DS, Developer etc.).
+#print("Programming is easy")
+#variable
+
+length = 100
+print(length)
+print(id(length)) #to check address of length and it stored in binary format
+breadth = 100
+print(breadth)
